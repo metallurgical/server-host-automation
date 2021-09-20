6a194f339316670050e01e6c5e4e50066cb6716f
Norlihazmey Ghazali <norlihazmey.ghazali@gmail.com>
2021-09-20 13:49:14 +0800

    auto get php fpm socket value

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 var domain, projectRoot, gitEndpoint, whichWebServer, phpVersion string
 
 func main() {
-	fmt.Println("[ Server host automation tool by @metallurgical(https://github.com/metallurgical) ]")
+	fmt.Println("[ Server host automation tool by @metallurgical(https://github.com/metallurgical)]")
 	askForInput()
 
 	if projectRoot != "" && gitEndpoint != "" {
@@ -38,9 +38,6 @@ func askForInput() {
 
 	fmt.Print("4) Which web server? (Apache - 1, Nginx - 2, key in 1 or 2) ? : ")
 	fmt.Scanln(&whichWebServer)
-
-	fmt.Print("5) Write down current server PHP version? (Leave empty if using apache) ? : ")
-	fmt.Scanln(&phpVersion)
 }
 
 func createNginxVhost() {
@@ -49,23 +46,37 @@ func createNginxVhost() {
 		return
 	}
 	// Get/download the file from source
-	cmd := exec.Command("wget", "https://raw.githubusercontent.com/metallurgical/server-host-automation/master/default-nginx-host.conf", "-P", "/tmp");
+	cmdWget := exec.Command("wget", "https://raw.githubusercontent.com/metallurgical/server-host-automation/master/default-nginx-host.conf", "-P", "/tmp");
+	cmdWget.Run();
 	// Copy the file from source into nginx sites-available folder
 	var vhostFileName = domain + ".conf"
 	var domainPath = "/etc/nginx/site-available/" + vhostFileName
-	cmd = exec.Command("cp", "/tmp/" + vhostFileName, domainPath)
-	cmd.Run()
+	cmdCp := exec.Command("cp", "/tmp/" + vhostFileName, domainPath)
+	cmdCp.Run();
 
 	// Replace document root full path into new project directory path
-	replaceContent(domainPath, "$documentRoot", projectRoot + "/public");
+	replaceContent(domainPath, "[documentRoot]", projectRoot + "/public");
 	// Replace matching server name with new the exact domain name
-	replaceContent(domainPath, "$serverName", domain);
+	replaceContent(domainPath, "[serverName]", domain);
+	// Get the full path of php-fpm socket. This will return the output
+	// something similar to this eg: "listen = /run/php/php7.4-fpm.sock"
+	cmdGetFpmPath, err := exec.Command(
+		"cat",
+		"/etc/php/$(php -r 'echo PHP_VERSION;' | grep --only-matching --perl-regexp '7.\\d+')/fpm/pool.d/www.conf",
+		"|",
+		"grep",
+		"'listen ='",
+		).Output()
 	// Replace php fpm socket path
-	replaceContent(domainPath, "$phpFpmSocket", "unix:/var/run/php/php " + phpVersion + "-fpm.sock");
+	replaceContent(domainPath, "[phpFpmSocket]", "unix:/var/" + string(cmdGetFpmPath)[9:]);
 
 	// Once successfully created into sites-available, create symlink to that file
-	cmd = exec.Command("ln", "-s", domainPath, "/etc/nginx/sites-enabled/")
-	cmd.Run()
+	cmdLn := exec.Command("ln", "-s", domainPath, "/etc/nginx/sites-enabled/")
+	cmdLn.Run()
+
+	// Restart nginx web server once done
+	cmdRestartWebServer := exec.Command("service", "nginx", "restart")
+	cmdRestartWebServer.Run();
 }
 
 func createApacheVhost() {
