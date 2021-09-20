781dde9c3bae6ce6497e593762fc081dcb00cab9
Norlihazmey Ghazali <norlihazmey.ghazali@gmail.com>
2021-09-20 12:50:07 +0800

    push to get default-nginx-host.conf full url path for copy later

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/metallurgical/server-host-automation
+
+go 1.17
--- main.go
+++ main.go
@@ -1,21 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
-var domain, projectRoot, gitEndpoint string;
+var domain, projectRoot, gitEndpoint, whichWebServer, phpVersion string
 
 func main() {
-	askForInput();
+	fmt.Println("[ Server host automation tool by @metallurgical(https://github.com/metallurgical) ]")
+	askForInput()
+
+	if projectRoot != "" && gitEndpoint != "" {
+		cmd := exec.Command("git", "clone", gitEndpoint, projectRoot)
+		cmd.Run();
+	}
 }
 
 func askForInput() {
-	fmt.Println("[ Server host automation tools by @metallurgical(https://github.com/metallurgical) ]");
-	fmt.Print("1) What is the domain name (without http/https) ? : ");
-	fmt.Scanln(&domain);
+	fmt.Print("1) What is the domain name (without http/https) ? : ")
+	fmt.Scanln(&domain)
 
-	fmt.Print("2) Where to clone the git repo (Full path to base folder of project) ? : ");
-	fmt.Scanln(&projectRoot);
+	fmt.Print("2) Where to clone the git repo (Full path to base folder of project) ? : ")
+	fmt.Scanln(&projectRoot)
 
-	fmt.Print("3) Full URL of git endpoint (will be automatically cloned into folder set in no 2) ? : ");
-	fmt.Scanln(&gitEndpoint);
+	fmt.Print("3) Full URL of git endpoint (will be automatically cloned into folder set in no 2) ? : ")
+	fmt.Scanln(&gitEndpoint)
+
+	fmt.Print("4) Which web server? (Apache - 1, Nginx - 2, key in 1 or 2) ? : ")
+	fmt.Scanln(&whichWebServer)
+
+	fmt.Print("5) Write down current server PHP version? (Leave empty if using apache) ? : ")
+	fmt.Scanln(&phpVersion)
+}
+
+func createNginxVhost() {
+	_, err := os.Stat("/etc/nginx/sites-available")
+	if err != nil {
+		return
+	}
+	var domainPath = "/etc/nginx/site-available/" + domain + ".conf"
+
+	cmd := exec.Command("ln", "-s", domainPath, "/etc/nginx/sites-enabled/")
+	cmd.Run()
+
+	cmd = exec.Command("ln", "-s", domainPath, "/etc/nginx/sites-enabled/")
+	cmd.Run()
 }
